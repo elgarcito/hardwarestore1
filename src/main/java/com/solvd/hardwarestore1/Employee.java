c889c20b9b94b69412e82d344184c587ff5f0f84
elgaricito <edgarjaguirre@hotmail.com>
2023-11-24 15:32:03 -0300

    Corrected try catch blocks as seen in classes

--- src/main/java/com/solvd/hardwarestore1/Employee.java
+++ src/main/java/com/solvd/hardwarestore1/Employee.java
@@ -111,8 +111,7 @@ public class Employee extends Person implements VacationAble {
     }
     //This method allows checking if the employee name is longer than the maximum it is used to
     // define the exception
-    public void throwNameException(String employeeName){
-        try {
+    public void throwNameException(String employeeName) throws LongEmployeeNameException,NumberInNamesException{
             if (employeeName.length()>20) {
                 throw new LongEmployeeNameException("The name is too long, pleas make it less than 20 characters.");
             }else if ((employeeName.contains("0") || employeeName.contains("1") || employeeName.contains("2") || employeeName.contains("3") || employeeName.contains("4") || employeeName.contains("5") || employeeName.contains("6") || employeeName.contains("7") || employeeName.contains("8") || employeeName.contains("9"))){
@@ -120,61 +119,49 @@ public class Employee extends Person implements VacationAble {
             }else{
                 LOGGER.info("The name length is ok and it has not any number inside");
             }
-        }catch (LongEmployeeNameException |NumberInNamesException e){
-            LOGGER.error("Something went wrong: "+e);
-        }
     }
 
     //This exception check Email
-    public void throwEmailException(String employeeEmail){
-        try {
+    public void throwEmailException(String employeeEmail) throws EmailException{
             if (!employeeEmail.contains("@gmail.com") ) {
                 throw new EmailException("Please only use a @gmail.com email.");
             } else {
                 LOGGER.info("The gmail format is OK, only gmail mails.");
             }
-        }catch (EmailException e ){
-            LOGGER.error("Something went wrong: "+e);
-        }
-
-
     }
     //This exception checks the phone number length
-    public void throwPhoneNumberException(String employeePhoneNumber){
-        try {
+    public void throwPhoneNumberException(String employeePhoneNumber) throws PhoneNumberException{
             if (employeePhoneNumber.length()==8) {
                 LOGGER.info("The 8 digit phone number is Ok.");
             } else {
                 throw new PhoneNumberException("The phone number has " + employeePhoneNumber.length()
                         + " digits and should be 8 digits");
             }
-        }catch (PhoneNumberException e){
-            LOGGER.error("Something went wrong: "+e);
-        }
     }
 
     //This exception checks the speciality
-    public void throwEmployeeSpecialityException(String employeeSpeciality){
-        try {
+    public void throwEmployeeSpecialityException(String employeeSpeciality) throws EmployeeSpecialityException{
             if (employeeSpeciality.equals("seller")|| employeeSpeciality.equals("owner") || employeeSpeciality.equals("deposit")) {
                 LOGGER.info("The specialty "+employeeSpeciality+" is Ok.");
             }else {
                 throw new EmployeeSpecialityException("The speciality"+ employeeSpeciality+" doesn't exist, please " +
                         "enter a valid one: seller or deposit.");
             }
-        }catch (EmployeeSpecialityException e ){
-            LOGGER.error("Something went wrong: "+e);
-        }
-
     }
 
     //Now the method that uses the try catch and in which we use the exception.
     // This is the one that is going to be used in Main
     public void checkEmployeeData(){
-        throwNameException(this.getPersonName());
-        throwEmailException(this.getPersonEmail());
-        throwPhoneNumberException(this.getPhoneNumber());
-        throwEmployeeSpecialityException(this.speciality);
+        try {
+            throwNameException(this.getPersonName());
+            throwEmailException(this.getPersonEmail());
+            throwPhoneNumberException(this.getPhoneNumber());
+            throwEmployeeSpecialityException(this.speciality);
+        }catch (EmployeeSpecialityException | EmailException |NumberInNamesException
+                |LongEmployeeNameException| PhoneNumberException e){
+            LOGGER.error(e);
+        }
+
     }
 
     //This method allows iterating in the employeeList and retrieving the names of the employees
